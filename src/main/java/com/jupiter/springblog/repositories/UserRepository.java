ae61547ad868716e618032a8076c05fff8c75ebd
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2021-02-23 14:38:53 -0600

    working thru exercises

--- src/main/java/com/jupiter/springblog/repositories/UserRepository.java
+++ src/main/java/com/jupiter/springblog/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package com.jupiter.springblog.repositories;
 import com.jupiter.springblog.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
