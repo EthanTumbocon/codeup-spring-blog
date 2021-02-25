b18d5e9c073544a9b7ba1d1efed5f782f8472997
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2021-02-25 11:58:00 -0600

    working thru exercises

--- src/main/java/com/jupiter/springblog/controllers/PostController.java
+++ src/main/java/com/jupiter/springblog/controllers/PostController.java
@@ -2,44 +2,24 @@ package com.jupiter.springblog.controllers;
 
 
 import com.jupiter.springblog.models.Post;
+import com.jupiter.springblog.models.User;
 import com.jupiter.springblog.repositories.PostRepository;
+import com.jupiter.springblog.services.EmailService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class PostController {
 
     private final PostRepository postsDao;
+    private final EmailService emailService;
 
-    public PostController(PostRepository postsDao) {
+    public PostController(PostRepository postsDao, EmailService emailService) {
         this.postsDao = postsDao;
+        this.emailService = emailService;
     }
-
-//    @GetMapping("/posts")
-//    public String postsIndex(Model model){
-//        Post post1 = new Post("First Post", "This is my first post", 1);
-//        Post post2 = new Post("Second Post", "This is my 2nd post", 2);
-//        Post post3 = new Post("Third Post", "This is my 3rd post", 3);
-//
-//        List<Post> postList = new ArrayList<>();
-//        postList.add(post1);
-//        postList.add(post2);
-//        postList.add(post3);
-//
-//        model.addAttribute("title", "All Posts");
-//        model.addAttribute("posts", postList);
-//
-//        return "posts/index";
-//    }
-
-    //new All Posts
 
     @GetMapping("/posts/jpa")
     @ResponseBody
@@ -47,15 +27,6 @@ public class PostController {
         return postsDao.findAll();
     }
 
-//    @GetMapping("/posts/{id}")
-//    public String postView(Model model){
-//        Post post = new Post("First Post", "This is my first post", 1);
-//        model.addAttribute("title", "Single Posts");
-//        model.addAttribute("post", post);
-//        return "posts/show";
-//    }
-
-    //new single post
 
     @GetMapping("/posts/jpa/{id}")
     @ResponseBody
@@ -72,7 +43,18 @@ public class PostController {
 
     @PostMapping("/posts/create")
     @ResponseBody
-    public String createPost(){
+    public String createPost(@ModelAttribute Post post){
         return "Creating a new post...";
+
+        Post savePost = postsDao.save(post);
+
+        String subject = "New Post Created!";
+        String body = "Dear" + savedPost.getUser.getUsername()
+                + ". Thank you for creating an ad. Your ad id is "
+                + savedPost.getId();
+        emailService.prepareAndSend(savedPost, subject, body);
+
+        return"redirect:/posts";
     }
+
 }
